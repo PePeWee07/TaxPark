b34037b39c91634b30c569da2bf0320bdfd92933
arnau <arnauroman13@gmail.com>
2022-10-31 21:46:46 +0100

    cv

--- src/app/login/login.page.spec.ts
+++ src/app/login/login.page.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { LoginPage } from './login.page';
+import { loginPage } from './login.page';
 
-describe('LoginPage', () => {
-  let component: LoginPage;
-  let fixture: ComponentFixture<LoginPage>;
+describe('loginPage', () => {
+  let component: loginPage;
+  let fixture: ComponentFixture<loginPage>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginPage ],
+      declarations: [ loginPage ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(LoginPage);
+    fixture = TestBed.createComponent(loginPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
