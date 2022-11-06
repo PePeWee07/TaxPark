d895ff1c29de274acc51b3ca29ab61fe6f7d559b
Pepe <95196085+PePeWee07@users.noreply.github.com>
2022-11-06 12:53:47 -0500

    Correccion fallo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,7 +20,7 @@ const routes: Routes = [
   },
   {
     path: 'login',
-    loadChildren: () => import('./iniciar sesion/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
   {
     path: 'register',
